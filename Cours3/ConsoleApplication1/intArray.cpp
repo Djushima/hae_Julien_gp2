ce93c6e0fdbab8c7789f66807837ebc31e61a01f
Djushima <55873109+Djushima@users.noreply.github.com>
2019-10-18 09:05:33 +0200

    Cours3

---
+++ Cours3/ConsoleApplication1/intArray.cpp
@@ -0,0 +1,98 @@
+
+#include "pch.h"
+#include "intArray.hpp"
+
+int intArray::TEST = 66;
+
+bool intArray::ensure(int size)
+{
+	if (maxSize > size) return true;
+
+	int * dataTemp = new int[size];
+	for (int i = 0; i < curSize; i++)
+	{
+		dataTemp[i] = data[i];
+	}
+	delete(data);
+	data = dataTemp;
+
+	maxSize = size;
+	printf("IN\n");
+	return true;
+}
+
+void intArray::push_back(int elem) {
+	ensure(curSize + 1);
+	data[curSize] = elem;
+	curSize++;
+}
+
+void intArray::push_first(int elem) {
+	ensure(curSize + 1);
+	for (int i = curSize; i > 0; i--) {
+		data[i] = data[i - 1];
+	}
+	data[0] = elem;
+	curSize++;
+}
+
+void intArray::insert(int pos, int elem) {
+	ensure( (pos >= curSize) ? (pos + 1) : (curSize + 1) );
+	for (int i = curSize; i > pos; i--) {
+		data[i] = data[i - 1];
+	}
+	data[pos] = elem;
+	if (pos >= curSize)
+		curSize = pos + 1;
+	else
+		curSize++;
+}
+
+int intArray::SearchPosition(int elem) { //TabTrié
+	for (int i = 0; i < curSize; i++) {
+		if (data[i] >= elem) return i;
+	}
+	return curSize;
+}
+
+void intArray::Remove(int elem) {
+	for (int i = 0; i < curSize; i++) {
+		if (data[i] == elem) {
+			for (int j = i; j < curSize; j++)
+				data[j] = (j + 1 < curSize ? data[j + 1] : NULL);
+			curSize--;
+			//break; Si tu veux enlever que la 1ere occurence.
+		}
+	}
+}
+
+void intArray::RemoveAll() {
+	for (int i = 0; i < curSize; i++) {
+		data[i] = NULL;
+	}
+	curSize = 0;
+}
+
+void intArray::fillWithRandom(int nbElem)
+{
+	RemoveAll();
+	ensure(nbElem);
+
+	for (int i = 0; i < nbElem; i++)
+	{
+		int val = std::rand() % 100;
+		set(i, val);
+	}
+}
+
+void intArray::Sort() {
+	intArray nuData;
+	for (int i = 0; i < curSize; i++) {
+		int val = get(i);
+		nuData.insert(nuData.SearchPosition(val), val);
+	}
+
+	for (int j = 0; j < curSize; j++) {
+		set(j, nuData.get(j));
+	}
+}
---
+++ Cours3/ConsoleApplication1/intArray.hpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+
+class intArray {
+
+public:
+
+	std::string name;
+	int * data; //Tableau dynamique qui contient les données
+	int maxSize = 0;
+	int curSize = 0;
+	//membre TEST statique (qui n'existe qu'à un seul endroit)
+	static int TEST;
+
+	intArray(int size = 1, const char * name = "") {
+		//on passe name en std::string
+		//alloue auto un char * et en fait la copie
+		this->name = name;
+		printf("Construite! %s\n", this->name.c_str());
+
+		//Version C Old school
+		//data = (int * )malloc(size * sizeof(int));
+		//memset(sata,0, size * sizeof(int));
+
+		//version C new school
+		//data = (int *)calloc(size, sizeof(int));
+		//calloc met des zero
+
+		//version c++ safe
+		data = new int[size];
+		for (int i = 0; i < size; ++i) data[i] = 0;
+
+		//curSize = 0;
+		maxSize = size;
+
+	}
+
+	~intArray() {
+
+		printf("Detruite! %s\n", name.c_str());
+	}
+
+	bool ensure(int size);
+
+	/*int operator() (int pos) {
+		ensure(pos+1);
+		return data[pos];
+	}
+
+	int & operator[] (int pos) {
+		ensure(pos+1);
+		return data[pos];
+	}*/
+
+	int getLength() {
+		return curSize;
+	}
+
+	int get(int pos) {
+		return data[pos];
+	}
+
+	void set(int pos, int elem) {
+		ensure(pos+1);
+		data[pos] = elem;
+		if (curSize <= pos)
+			curSize = pos + 1;
+	}
+
+	void set_unsafe(int pos, int elem) {
+		data[pos] = elem;
+		curSize = pos + 1;
+	}
+
+	void push_back(int elem);
+
+	void push_first(int elem);
+
+	void insert(int pos, int elem);
+
+	int SearchPosition(int elem);
+
+	void Remove(int elem);
+
+	void RemoveAll();
+
+	void fillWithRandom(int nbElem);
+
+	void Sort();
+};
